7c57c52d804f3379d9aae50bac26556594c759c6
CrowTrooper202 <adamb88cke@gmail.com>
2023-04-11 18:09:18 -0400

    added inquirer questions and calls fs to js

--- index.js
+++ index.js
@@ -1,7 +1,64 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer')
+const fs = require('fs')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What do want as the name of the ReadMe?',
+            name: 'fileName',
+        },
+        {
+            type: 'input',
+            message: 'What do want in the discription section?',
+            name: 'discription', 
+        },
+        {
+            type: 'input',
+            message: 'What do want in the installation section?',
+            name: 'installation',
+        },
+        {
+            type: 'input',
+            message: 'What do want in the Useage section?',
+            name: 'usage',
+        },
+        {
+            type: 'input',
+            message: 'What do want in the guide line section?',
+            name: 'guideLine',
+        },
+        {
+            type: 'input',
+            message: 'What do want in the contribution section?',
+            name: 'contribution',
+        },
+        {
+            type: 'input',
+            message: 'What do want in the test section?',
+            name: 'test',
+        },
+        {
+            type: 'input',
+            message: 'What is you GitHub username?',
+            name: 'installation',
+        },
+        {
+            type: 'input',
+            message: 'What is your email?',
+            name: 'email',
+        },
+        {
+            type: 'list',
+            message: 'What license do you want to use?',
+            name: 'license',
+            choices: ['MIT', 'Apache', 'Boost Software', "creative Commons Zero", 'None']
+        },
+        
+    ])
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
