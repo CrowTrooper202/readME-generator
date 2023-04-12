6e4d001a130c0c061737603fe95a8bcfb2f2e4cb
CrowTrooper202 <adamb88cke@gmail.com>
2023-04-12 14:46:35 -0400

    added readme format

--- index.js
+++ index.js
@@ -83,20 +83,3 @@ function init() {
 
 // Function call to initialize app
 init();
-
-// switch (data.license) {
-//     case 'MIT':
-//         // fs.appendFile(`${name}`, "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)" );
-//         break;
-//     case 'Apache':
-//         // fs.appendFile('fileName.md', '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)');
-//         break;
-//     case 'Boost Software':
-//         // fs.appendFile('fileName.md', '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)');
-//         break;
-//     case 'creative Commons Zero':
-//         // fs.appendFile('fileName.md', '[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)');
-//         break;
-//     default:
-//         break;
-// }
